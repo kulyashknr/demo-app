35aecd0a3f09602f04a8d8667aa1933ea4aa496a
Kulyash Konyrova <konyrova@gmail.com>
2020-08-15 01:07:30 +0600

    listing all news

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -28,4 +29,8 @@ class ArticleViewSet(mixins.CreateModelMixin,
     @action(methods=['DELETE'], detail=True)
     def perform_destroy(self, instance):
         instance.delete()
+
+    def get(self):
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response(serializer.data)
 
